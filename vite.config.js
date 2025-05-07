ce8db3a3c279061c925d24b993ddc0dd5a9bd4f3
jshmitty88 <justinschmidt@netrevenue.io>
2025-05-07 02:38:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true
+  },
+  preview: {
+    port: process.env.PORT,
+    host: true,
+    strictPort: true,
+    allowedHosts: ['anchorai.up.railway.app']
+  }
 })

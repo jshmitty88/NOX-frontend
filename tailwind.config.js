685e8a832587511524a1b7ef065dbb93bb401664
jshmitty88 <justinschmidt@netrevenue.io>
2025-05-07 01:32:46 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#4994D1',
+        userBubble: 'rgba(255,255,255,0.2)',
+        white: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}

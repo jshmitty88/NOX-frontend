57f552d8f8e33973bf4cc8f0f8535c91f4db7da0
jshmitty88 <justinschmidt@netrevenue.io>
2025-05-07 20:27:06 -0400

    Update MessageInput.jsx

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -52,7 +52,7 @@ function MessageInput({ onSend }) {
         {/* Send Button */}
         <button
           onClick={handleSend}
-          className="w-10 h-10 flex items-center justify-center rounded-full bg-noxBlue/20 text-black text-xl font-bold leading-[1] hover:bg-noxBlue/30"ver:bg-noxBlue/10"
+          className="w-10 h-10 flex items-center justify-center rounded-full bg-noxBlue/20 text-black text-xl font-bold leading-[1] hover:bg-noxBlue/30"
         >
           ↑
         </button>
